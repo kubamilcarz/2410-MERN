329cf2141f2b3c11ea787ef1210747d057eb9101
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-04 15:12:46 +0200

    add note modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,19 @@ import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Note as model } from './models/note';
 import Note from './components/Note';
 import styles from './styles/NotesPage.module.css';
+import stylesUtils from './styles/utils.module.css';
+import * as notesAPI from "./network/notes_api";
+import AddNoteDialog from './components/AddNoteDialog';
 
 function App() {
 	const [notes, setNotes] = useState<model[]>([]);
 
+	const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
 	useEffect(() => {
 		async function loadNotes() {
 			try {
-				const response = await fetch("/api/notes", { method: "GET" });
-
-				const notes = await response.json();
+				const notes = await notesAPI.fetchNotes();
 				setNotes(notes);
 			} catch (error) {
 				console.error(error);
@@ -26,6 +29,10 @@ function App() {
 
 	return (
 		<Container>
+			<Button 
+				className={`mb-4 ${stylesUtils.blockCenter}`}
+				onClick={() => setShowAddNoteDialog(true)}>Add new note</Button>
+
 			<Row xs={1} md={2} xl={3} className='g-4'>
 				{notes.map(note => (
 					<Col key={note._id}>
@@ -33,6 +40,15 @@ function App() {
 					</Col>
 				))}
 			</Row>
+
+			{ showAddNoteDialog && 
+				<AddNoteDialog 
+					onDismiss={() => setShowAddNoteDialog(false)} 
+					onNoteSaved={(newNote) => {
+						setNotes([...notes, newNote])
+						setShowAddNoteDialog(false)
+					}} /> 
+			}
 		</Container>
 	);
 }
